2eb9a07476a68b77117966420def383d11e372bd
DaviSantos <davicalixto2077@gmail.com>
2022-09-06 20:53:24 -0300

    tests in project

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ public class Program {
     public static void main(String[] args) {
 	
 	System.out.println("Hellow world!");
+	System.out.println("Hellow world!");
+	System.out.println("Hellow world!");
 
     }
 
