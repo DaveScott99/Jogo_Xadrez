1a015c7f587acc014c457e2be00ad82e29628fba
DaviSantos <davicalixto2077@gmail.com>
2022-09-08 23:49:48 -0300

    Check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -28,7 +28,7 @@ public class ChessPosition {
     }
     
     protected static ChessPosition fromPostion(Position position) {
-	return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
     }
     
     @Override
